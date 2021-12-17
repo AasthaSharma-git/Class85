09644191e055d33c20f0a7984495ba90286ebe17
AasthaSharma-git <67312804+AasthaSharma-git@users.noreply.github.com>
2021-12-17 11:10:49 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+
+
+// Your web app's Firebase configuration
+export const firebaseConfig = {
+  apiKey: "AIzaSyDLPr2iqqQUgV7fV9ln-jN7I8qSsAoViHk",
+  authDomain: "story-telling-app-c70a9.firebaseapp.com",
+  projectId: "story-telling-app-c70a9",
+  storageBucket: "story-telling-app-c70a9.appspot.com",
+  messagingSenderId: "587246002848",
+  appId: "1:587246002848:web:86eeda581d2097b3639fc5"
+};
+
+// Initialize Firebase
